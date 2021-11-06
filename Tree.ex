33c155a6f5692570667d1fa984c43f012743ee9c
nick-kano <nick.kano@ciencias.unam.mx>
2021-11-06 15:59:37 -0600

    avance broadcast

--- Tree.ex
+++ Tree.ex
@@ -6,9 +6,27 @@ defmodule Tree do
 
   defp loop() do
     receive do
-      {:broadcast, tree, i, caller} -> :ok #Aquí va su código.
+      {:broadcast, tree, i, caller} ->
+        if esHoja(i) do
+          send(caller,{:acabe})
+        else
+          if hayHijoIzq?(i,tree) do
+            send(Map.fetch(tree,2*i+1),{:broadcast, tree, 2*i+1, caller})
+          end
+          if hayHijoDer?(i,tree) do
+            send(Map.fetch(tree,2*i+2),{:broadcast, tree, 2*i+2, caller})
+          end
+        end
       {:convergecast, tree, i, caller} -> :ok #Aquí va su código.
     end
+  end
+
+  defp hayHijoIzq(int, tree) do
+    Map.has_key(tree,2*int+1)
+  end
+
+  defp hayHijoDer(int, tree) do
+    Map.has_key(tree,2*int+2)
   end
 
   defp create_tree([], tree, _) do
@@ -20,8 +38,19 @@ defmodule Tree do
   end
 
   def broadcast(tree, n) do
-    #Aquí va su código.
-    :ok
+    hojas=div((2*n+1)/2,1)
+    {:ok,pid}=Task.start(fn -> mensajes(hojas) end)
+    send(Map.fetch(tree,0),{:broadcast, tree, 0, pid})
+  end
+
+  defp mensajes(n) do
+    if n==0 do
+      :broadcast_over
+    else
+      receive do
+        {:acabe}->mensajes(n-1)
+      end
+    end
   end
 
   def convergecast(tree, n) do
