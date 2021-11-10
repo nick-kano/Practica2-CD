0618c20815a0e58ab7c7eb72d12289dfa5941e94
paoinnesb <paoinnes@ciencias.unam.mx>
2021-11-09 22:08:49 -0600

    bfs listo :)

--- Graph.ex
+++ Graph.ex
@@ -1,17 +1,38 @@
 defmodule Graph do
-  
+
   def new(n) do
-    create_graph(Enum.map(1..n, fn _ -> spawn(fn -> loop(-1) end) end), %{}, n)
+    control = spawn(fn -> controller(%{}, n) end)
+    create_graph(Enum.map(1..n, fn _ ->
+      spawn(fn ->
+        loop(-1, control)
+      end)
+    end), %{}, n)
   end
 
-  defp loop(state) do
+  defp loop(state, controller) do
     receive do
-      {:bfs, graph, new_state} -> :ok
+      {:bfs, graph, new_state} ->
+        if new_state > state and state != -1 do
+          send(controller, {:done, self(), state});
+          Process.sleep(5000)
+        else
+          state =
+            cond do
+              new_state < state -> new_state
+              state == -1 -> new_state
+              true -> state
+            end
+          vecinos = Map.get(graph, self())
+          Enum.map(vecinos, fn vecino -> send(vecino, {:bfs, graph, state + 1}) end)
+          loop(state, controller)
+        end
       {:dfs, graph, new_state} -> :ok
-      {:get_state, caller} -> send(caller, {self, state}) #Estos mensajes solo los manda el main.
+      {:get_state, caller} -> #Estos mensajes solo los manda el main.
+
+        send(caller, {self, state})
     end
   end
-  
+
   defp create_graph([], graph, _) do
     graph
   end
@@ -36,27 +57,45 @@ defmodule Graph do
       u == nil or v == nil -> graph
       u == v -> graph
       true ->
-	u_neighs = Map.get(graph, u)
-	new_u_neighs = MapSet.put(u_neighs, v)
-	graph = Map.put(graph, u, new_u_neighs)
-	v_neighs = Map.get(graph, v)
-	new_v_neighs = MapSet.put(v_neighs, u)
-	Map.put(graph, v, new_v_neighs)
+      	u_neighs = Map.get(graph, u)
+      	new_u_neighs = MapSet.put(u_neighs, v)
+      	graph = Map.put(graph, u, new_u_neighs)
+      	v_neighs = Map.get(graph, v)
+      	new_v_neighs = MapSet.put(v_neighs, u)
+      	Map.put(graph, v, new_v_neighs)
     end
   end
 
   def random_src(graph) do
     Enum.random(Map.keys(graph))
   end
-  
+
   def bfs(graph, src) do
-    :ok
+    IO.puts("START:")
+    IO.puts(inspect(src))
+
+    send(src, {:bfs, graph, 0})
+
+  end
+
+  def controller(map, n) do #solo para mostrar los procesos cuando ya terminen
+    receive do
+      {:done, process, state} ->
+        new = Map.put(map, process, state)
+        IO.puts(inspect(process))
+        IO.puts state
+        if map_size(new) == n do
+          IO.puts("termin[e]")
+        else
+          controller(new, n)
+        end
+    end
   end
 
   def bfs(graph) do
     bfs(graph, random_src(graph))
   end
-    
+
   def dfs(graph, src) do
     :ok
   end
@@ -64,5 +103,5 @@ defmodule Graph do
   def dfs(graph) do
     dfs(graph, random_src(graph))
   end
-  
+
 end
