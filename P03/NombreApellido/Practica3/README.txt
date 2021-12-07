69098964bae6e2b605fd11e86beacba048e5f186
Sebastián <alamina@ciencias.unam.mx>
2021-12-06 21:48:02 -0600

    P03 | Avances.

--- P03/NombreApellido/Practica3/README.txt
+++ P03/NombreApellido/Practica3/README.txt
@@ -4,3 +4,23 @@ Fernanda Garduño Ballesteros		317010316
 Sebastián Alamina Ramírez			318685496
 Carmen Paola Innes Barrón			317346741
 Jean Durán Villanueva				316032416
+
+--- EJECUCIÓN ---
+1) Dentro de la carpeta "src", ejecutar el
+archivo "Consensus.ex" desde el intérprete
+de Elixir mediante el comando "iex Consensus.ex".
+
+2) Ejecutar la función "Consensus.consensus(n)",
+en donde "n" es el número de procesos a pariticipar
+durante el consenso.
+
+*) Si se desea más información durante la ejecución
+del programa (por ejemplo, para depurar), es posible
+descomentar las dos líneas dentro de la condición
+"if(state == :fail) do" al principio de la función
+privada "loop/4".
+
+--- NOTAS ---
+* Se agregó un parámetro a la función loop para
+que los procesos puedan llevar un seguimiento
+respecto a quiénes son sus vecinos.
--- P03/NombreApellido/src/Consensus.ex
+++ P03/NombreApellido/src/Consensus.ex
@@ -10,56 +10,99 @@ defmodule Consensus do
   def create_consensus(n) do
 
     # La función anónima de cada proceso será
-    # loop(:start, 0, probabilidad_de_ser_un_proceso_fallido).
+    # loop(:start, 0, probabilidad_de_ser_un_proceso_fallido, lista de vecinos).
     Enum.map(1..n, fn _ ->
-      spawn(fn -> loop(:start, 0, :rand.uniform(10)) end)
+      spawn(fn -> loop(:start, 0, :rand.uniform(10), []) end)
     end)
 
-    #Agregar código aquí es válido.
+    # Agregar código aquí es válido.
+    # Sin embargo, requerimos que el Enum.map
+    # sea lo último, para devolver una lista.
 
   end # <- Fin de la función create_consensus/1.
 
   # Función loop. Es la función
   # que cada proceso ejecuta.
-  defp loop(state, value, miss_prob) do
+  defp loop(state, value, miss_prob, neighbours) do
 
     # Inicia código inamovible...
     if(state == :fail) do
-      loop(state, value, miss_prob) # Un proceso fallido se queda estancado aquí uwu.
+      # IO.puts ( "Fallé uwu soy el proceso de ID " <> inspect(self()) ) # CÓDIGO TEMPORAL PARA DEPURAR.
+      # Process.exit(self(), :fail) # CÓDIGO TEMPORAL PARA DEPURAR.
+      loop(state, value, miss_prob, neighbours) # Un proceso fallido se queda estancado aquí uwu.
     end
     # Termina código inamovible.
 
     receive do
       {:get_value, caller} -> send(caller, value) # No modificar.
       # Aquí se pueden definir más mensajes.
+      {:neighbours, list} -> loop(state, value, miss_prob, list) # Define la lista de procesos de este proceso.
+      {:set_value, x} -> if x < value do # Recepción del "value" de algún otro proceso. Continúo con el mínimo.
+        loop(state, x, miss_prob, neighbours)
+      else
+        loop(state, value, miss_prob, neighbours)
+      end
     after
-      1000 -> IO.puts("Analizar esto.") #:ok # Analizar porqué está esto aquí.
+      1000 -> :ok # Analizar por qué está esto aquí.
+      # Si esto no estuviera aquí, los procesos se quedarían estancados
+      # dentro de este "receive do" desde el principio; es decir, desde
+      # antes de siquiera cambiar su "state" que inició en :start y, por
+      # lo tanto, ni decidirían su valor aleatorio entre 1 y 10,000.
     end
 
     case state do
       :start -> # Inicialmente, hacer...
         chosen = :rand.uniform(10000) # ...escoger un valor aleatorio entre 1 y 10,000.
         if(rem(chosen, miss_prob) == 0) do # ...decidir si soy un proceso fallido o no.
-          loop(:fail, chosen, miss_prob) # ...en caso de que sí; fallar.
+          loop(:fail, chosen, miss_prob, neighbours) # ...en caso de que sí; fallar.
         else
-          loop(:active, chosen, miss_prob) # ...en caso de que no; continuar.
+          loop(:active, chosen, miss_prob, neighbours) # ...en caso de que no; continuar.
         end
-      :fail -> loop(:fail, value, miss_prob) # Si soy fallido; fallar.
-      :active -> :ok # Aquí va su código (de proceso correcto).
+      :fail -> loop(:fail, value, miss_prob, neighbours) # Si soy fallido; fallar.
+      :active -> for p <- neighbours do # Si soy un proceso correcto, por cada proceso vecino...
+        send(p, {:set_value, value}) # ...le envío mi "value".
+      end
+        loop(:sent, value, miss_prob, neighbours) # Y continúo mi ejecución como proceso que ya mandó su "value".
+      :sent -> loop(state, value, miss_prob, neighbours) # Loopeo para seguir recibiendo valores de mis vecinos.
 
     end # <- Fin del "case state do".
 
-  end # <- Fin de la función loop/3.
+  end # <- Fin de la función loop/4.
 
   # Función consensus. Regresa el valor
   # final escogido de manera unánime por
   # todos los hilos.
   def consensus(processes) do
-    #Process.sleep(10000)
-    # Aquí va su código, deben de regresar el valor
-    # unánime decidido por todos los procesos.
-    create_consensus(3)
-    #:ok
+
+    # Creamos tantos procesos como se indica en la función,
+    # y almacenamos en una lista "l" los PID's.
+    l = create_consensus(processes)
+
+    # Le decimos a cada proceso quiénes son sus vecinos.
+    # Estamos creando una gráfica completa.
+    for p <- l do
+      send(p, {:neighbours, List.delete(l,p)})
+    end
+
+    # Esperamos al consenso uwu.
+    IO.puts("Consenso en proceso...")
+    Process.sleep(5000) # Modificar la cantidad de milisegundos según el funcionamiento del código.
+    IO.puts("Consenso terminado.")
+
+    # Observamos el valor decidido de cada proceso.
+    for p <- l do # Por cada proceso que se creó...
+      send(p, {:get_value, self()}) # ...le pedimos su valor escogido.
+      receive do
+        # ...y lo imprimimos.
+        value -> IO.puts( "El proceso de ID " <> inspect(p) <> " decidió el valor: " <> inspect(value) )
+      after
+        # Si no hay respuesta, tenemos un proceso fallido.
+        100 -> IO.puts( "El proceso de ID " <> inspect(p) <> " falló." )
+      end
+    end
+
+    "Fin de la ejecución."
+
   end # <- Fin de la función consensus/1.
 
 end # <- Fin del módulo Consensus.
