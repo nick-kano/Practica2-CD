d5d545111898a584ce8646666cf50b03e882dafb
nick-kano <nick.kano@ciencias.unam.mx>
2022-01-16 15:16:33 -0600

    subo algoritmo principal acabado

--- Practica4/SlidingWindow.ex
+++ Practica4/SlidingWindow.ex
@@ -16,11 +16,55 @@ defmodule SlidingWindow do
   end
 
   def sender_loop(package, n, k) do
-    :ok
+    receive do
+      {:start,recvr}->send_pck(package,0,k,n,recvr)
+                      sender_loop2(package,n,k,Enum.map(1..n, fn x->0 end))
+    end
   end
 
+  defp sender_loop2(package,n,k,received)do
+    receive do
+      {:got,id,recvr}->received=List.replace_at(received,id,1)
+                 window_at=next_position(received,0)
+                 send_pck(package,window_at,k,n,recvr)
+                 if window_at>=n do
+                   send(recvr,{:finish})
+                 else                             
+                    sender_loop2(package,n,k,received)
+                 end
+    end
+  end
+
+  defp next_position([h|tail],i) do
+    if h==0 do
+      i
+    else
+      next_position(tail,i+1)
+    end
+  end
+
+  defp next_position([],i) do
+    i
+  end
+  defp send_pck(pck,id,k,tam,receiver)do
+    if k>0&&id<tam do
+      send(receiver,{:pack,id,Enum.at(pck,id)})
+      send_pck(pck,id+1,k-1,tam,receiver)
+    end
+  end
+
+
   def recvr_loop(sender) do
-    :ok
+    {:ok,pid}=Task.start(fn -> build_pck(sender,%{}) end)
+    send(sender,{:start,pid})
   end
   
+  defp build_pck(sender,map)do
+    this=self()
+    receive do
+      {:pack,id,v}->send(sender,{:got,id,this})
+                    build_pck(sender,Map.put(map,id,v))
+      {:finish}-> IO.inspect(map)
+    end
+  end
 end
